78769a67685e95f305227d51cb6f04a4e1083384
Morgan Meliment <morganmeliment@gmail.com>
2015-12-08 17:28:39 -0500

    Updated from Brython Server: December 8, 2015 5:28:40 PM EST

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Sources: http://www.tutorialspoint.com/python/list_remove.htm
+"""
 line1 = "gryyby" #input("First Line:")
 line2 = "prgpbb" #input("Second Line:")
 line3 = "bbggpb" #input("Third Line:")
@@ -25,8 +28,35 @@ def findsurrounding(row, index, color):
         finalret.append([row, index + 1])
     else:
         finalret.append([])
-    
-    return finalret
+    top = ""
+    right = ""
+    bottom = ""
+    left = ""
+    itera = 0
+    selfpotentialcombos = []
+    for pos in finalret:
+        itera += 1
+        if itera == 1:
+            if pos != []:
+                top = list(grid[pos[0]])[pos[1] - 1]
+        elif itera == 2:
+            if pos != []:
+                bottom = list(grid[pos[0]])[pos[1] - 1]
+        elif itera == 3:
+            if pos != []:
+                left = list(grid[pos[0]])[pos[1] - 1]
+        elif itera == 4:
+            if pos != []:
+                right = list(grid[pos[0]])[pos[1] - 1]
+    if top == color:
+        selfpotentialcombos.append([row - 1, index])
+    if right == color:
+        selfpotentialcombos.append([row, index + 1])
+    if bottom == color:
+        selfpotentialcombos.append([row + 1, index])
+    if left == color:
+        selfpotentialcombos.append([row, index - 1])
+    return selfpotentialcombos
 
 potentialcombos = []
 
@@ -34,43 +64,20 @@ dotnum = 0
 dotrow = 0
 for row in grid:
     dotrow += 1
-    stringrow = list(grid[row])
-    for dot in stringrow:
+    for dot in list(grid[row]):
         dotnum = (dotnum % 6) + 1
-        surrounding = findsurrounding(dotrow, dotnum)
-        top = ""
-        right = ""
-        bottom = ""
-        left = ""
-        itera = 0
-        selfpotentialcombos = []
-        for pos in surrounding:
-            itera += 1
-            if itera == 1:
-                if pos != []:
-                    top = list(grid[pos[0]])[pos[1] - 1]
-            elif itera == 2:
-                if pos != []:
-                    bottom = list(grid[pos[0]])[pos[1] - 1]
-            elif itera == 3:
-                if pos != []:
-                    left = list(grid[pos[0]])[pos[1] - 1]
-            elif itera == 4:
-                if pos != []:
-                    right = list(grid[pos[0]])[pos[1] - 1]
-        if top == dot:
-            selfpotentialcombos.append([dotrow - 1, dotnum])
-        if right == dot:
-            selfpotentialcombos.append([dotrow, dotnum + 1])
-        if bottom == dot:
-            selfpotentialcombos.append([dotrow + 1, dotnum])
-        if left == dot:
-            selfpotentialcombos.append([dotrow, dotnum - 1])
-        finalsurrounding = []
-        for combodot in selfpotentialcombos:
-            nextsurrounding = findsurrounding(combodot[0], combodot[1])
+        surrounding = findsurrounding(dotrow, dotnum, dot)
+        stringofdots = []
+        for combodot in surrounding:
+            stringofdots = [[dotrow, dotnum], combodot]
+            nextsurrounding = findsurrounding(combodot[0], combodot[1], dot)
+            nextsurrounding.remove([dotrow, dotnum])
+            for anotherdot in nextsurrounding:
+                stringofdots = [[dotrow, dotnum], combodot, anotherdot]
+                nnextsurrounding = findsurrounding(anotherdot[0], anotherdot[1], dot)
+                nnextsurrounding.remove([dotrow, dotnum])
 
-print(potentialcombos)
+#print(potentialcombos)
 
 
 
