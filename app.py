a6766e6d237c4dc82961cae4ab1a78b04adae9bd
Morgan Meliment <morganmeliment@gmail.com>
2015-12-08 17:01:22 -0500

    Updated from Brython Server: December 8, 2015 5:01:23 PM EST

--- app.py
+++ app.py
@@ -37,6 +37,73 @@ for row in grid:
     for dot in stringrow:
         dotnum = (dotnum % 6) + 1
         surrounding = findsurrounding(dotrow, dotnum)
+        top = ""
+        right = ""
+        bottom = ""
+        left = ""
+        itera = 0
+        selfpotentialcombos = []
         for pos in surrounding:
-            top = grid[pos[0]]
-            print(top)
+            itera += 1
+            if itera == 1:
+                if pos != []:
+                    top = list(grid[pos[0]])[pos[1] - 1]
+            elif itera == 2:
+                if pos != []:
+                    bottom = list(grid[pos[0]])[pos[1] - 1]
+            elif itera == 3:
+                if pos != []:
+                    left = list(grid[pos[0]])[pos[1] - 1]
+            elif itera == 4:
+                if pos != []:
+                    right = list(grid[pos[0]])[pos[1] - 1]
+        if top == dot:
+            selfpotentialcombos.append([[dotrow, dotnum], [dotrow - 1, dotnum]])
+        if right == dot:
+            selfpotentialcombos.append([[dotrow, dotnum], [dotrow, dotnum + 1]])
+        if bottom == dot:
+            selfpotentialcombos.append([[dotrow, dotnum], [dotrow + 1, dotnum]])
+        if left == dot:
+            selfpotentialcombos.append([[dotrow, dotnum], [dotrow, dotnum - 1]])
+        
+
+print(potentialcombos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
